c23424392a7d161bf8243bb61e16da9b690c8632
ss_ <sebacalvi08@gmail.com>
2026-05-20 08:46:52 -0300

    arreglo perfil rinoceronte y empiezo a implementar stories (no anda)

--- react/src/publicaciones.jsx
+++ react/src/publicaciones.jsx
@@ -1,10 +1,12 @@
 import Publicacion from './publicacion.jsx';
 import './publicaciones.css';
+import Stories from './stories.jsx';
 
 function Publicaciones() {
     return (
         <>
             <div class = "Publicaciones">
+                {/* <Stories /> */}
                 <Publicacion />
             </div>
         </>
--- react/src/stories.jsx
+++ react/src/stories.jsx
@@ -18,3 +18,4 @@ function Stories() {
     </>
   );
 }
+export default Stories;
